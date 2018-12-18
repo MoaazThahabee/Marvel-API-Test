2464d2e79a749d6b55e9ed97ea0e5f2a2ac61c51
Moaaz <moaaz.al.thahabee@gmail.com>
2018-12-18 23:19:31 +0200

    + Project Init

---
+++ Marvel Api Test/Marvel Api Test/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Marvel Api Test
+//
+//  Created by Moaaz Al-Thahabee on 12/17/18.
+//  Copyright © 2018 Moaaz Al-Thahabee. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
