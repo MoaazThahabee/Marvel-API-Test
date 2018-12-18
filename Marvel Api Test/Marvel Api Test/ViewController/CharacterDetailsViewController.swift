0cd01d60440942069db4dfc93768f1b90abedfb8
Moaaz <moaaz.al.thahabee@gmail.com>
2018-12-18 23:56:41 +0200

    * Rename DetailViewController -> CharacterDetailsViewController

--- Marvel Api Test/Marvel Api Test/ViewController/CharacterDetailsViewController.swift
+++ Marvel Api Test/Marvel Api Test/ViewController/CharacterDetailsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class CharacterDetailsViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
--- Marvel Api Test/Marvel Api Test/ViewController/CharactersListViewController.swift
+++ Marvel Api Test/Marvel Api Test/ViewController/CharactersListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class CharactersListViewController: UITableViewController {
 
-    var detailViewController: DetailViewController? = nil
+    var detailViewController: CharacterDetailsViewController? = nil
     var objects = [Any]()
 
 
@@ -23,7 +23,7 @@ class CharactersListViewController: UITableViewController {
         navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
-            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? CharacterDetailsViewController
         }
     }
 
@@ -45,7 +45,7 @@ class CharactersListViewController: UITableViewController {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
                 let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                let controller = (segue.destination as! UINavigationController).topViewController as! CharacterDetailsViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
